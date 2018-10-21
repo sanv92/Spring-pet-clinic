48caf38cbdecd3175a16da5ecaf7ff8f3dd6eaca
SanderV1992 <sander.vergeles@gmail.com>
2018-10-21 19:05:49 +0300

    owner - mvc mock test

--- pet-clinic-web/src/test/java/com/example/demo/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/example/demo/controllers/OwnerControllerTest.java
@@ -7,12 +7,21 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import com.example.demo.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,6 +32,9 @@ class OwnerControllerTest {
 
     @Mock
     Model model;
+
+    @Autowired
+    MockMvc mvc = null;
 
     OwnerController controller;
 
@@ -56,5 +68,14 @@ class OwnerControllerTest {
 
         Set<Owner> setInController = argumentCaptor.getValue();
         assertEquals(2, setInController.size());
+    }
+
+    @Test
+    void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/index"));
     }
 }
